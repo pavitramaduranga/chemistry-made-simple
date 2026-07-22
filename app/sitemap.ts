7d2510d18fc7b557104cfd0f8635e900f8ded28c
Pavitra Maduranga <pavitramaduranga@gmail.com>
2026-07-22 10:04:06 +0530

    Add app/sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,3 @@
+import type { MetadataRoute } from "next";
+import { articles, pages, site } from "./site-data";
+export default function sitemap():MetadataRoute.Sitemap { const routes=["",...Object.keys(pages),"chemistry-guides",...Object.keys(articles).map(x=>`chemistry-guides/${x}`)]; return routes.map((r,i)=>({url:`${site.domain}/${r}${r?"/":""}`,changeFrequency:i===0?"weekly":"monthly",priority:i===0?1:r.includes("chemistry-guides/")?.7:.8})); }
